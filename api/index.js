9e723989569d52148ab5c9382dada71af3aff670
Siri503 <sirikanthiachanta@gmail.com>
2025-01-03 21:40:46 +0530

    add reinstallation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import stockRouter from './routes/stock.route.js';
 import cookieParser from 'cookie-parser';
+import path from './path';
 dotenv.config();
 mongoose.connect(process.env.MONGO)
 .then(()=>{
@@ -13,6 +14,8 @@ mongoose.connect(process.env.MONGO)
 .catch((err)=>{
     console.log('Error connecting to MongoDB',err);
 });
+const __dirname=path.resolve();
+
 const app=express();
 app.use(express.json())
 app.use(cookieParser())
@@ -20,6 +23,12 @@ app.use(cookieParser())
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use("/api/stocks",stockRouter);
+
+app.use(express.static(path.join(__dirname,'/client/dist')));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html')); 
+ })
 
 app.use((err,req,res,next)=>{
     const statusCode=err.statusCode||500;
